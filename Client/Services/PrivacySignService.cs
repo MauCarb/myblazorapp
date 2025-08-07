f7e08ab861e3fdfbdf4be817c6b21406ead45b9d
MauCarb <maurizio.carbonaro@phoenix-im.it>
2025-08-07 08:02:46 +0200

    07/08/2025 08:02

---
+++ Client/Services/PrivacySignService.cs
@@ -0,0 +1,28 @@
+﻿using BlazorApp.Shared;
+using System.Net.Http.Json;
+
+public class PrivacySignService
+{
+    private readonly HttpClient _httpClient;
+
+    public PrivacySignService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<PrivacySign> AddPrivacySign(PrivacySign stuffsign)
+    {
+        var response = await _httpClient.PostAsJsonAsync("https://ophthalapi02-dcafg8d7hhekfkb0.italynorth-01.azurewebsites.net/api/privacysign", stuffsign);
+
+        // Assicurati che la risposta sia valida
+        response.EnsureSuccessStatusCode();
+
+        // Leggi e restituisci l'oggetto PrivacySign creato
+        return await response.Content.ReadFromJsonAsync<PrivacySign>();
+    }
+
+    public async Task DeletePrivacySign(decimal id)
+    {
+        await _httpClient.DeleteAsync($"https://ophthalapi02-dcafg8d7hhekfkb0.italynorth-01.azurewebsites.net/api/privacysign/{id}");
+    }
+}
---
+++ Shared/PrivacySign.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp.Shared
+{
+    [Table("PrivacySign")]
+    public class PrivacySign
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public decimal IdSign { get; set; }
+        public decimal ptIdPatient { get; set; }
+        public string SignGuid { get; set; }
+    }
+}
